42282d8debfd6a184e0a8f6230488b2821abcc73
Dakota Nelson <kotalilyy@gmail.com>
2021-08-19 07:50:33 -0500

    create board variable

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Board.css";
 
+const BOARD_SIZE = 10;
+
 const Board = () => {
-    return <div>
-        foo bar
-    </div>;
+  const [board, setBoard] = useState(
+    new Array(BOARD_SIZE).fill(0).map((row) => new Array(BOARD_SIZE).fill(0))
+  );
+
+  return (
+    <div className="board">
+      {board.map((row, rowIdx) => (
+        <div key={rowIdx} className="row">
+          {row.map((cell, cellIdx) => (
+            <div key={cellIdx} className="cell"></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Board;
